3c32fa2c33cd3375a80a125b4ac212fa6c984766
andrewekhalel <andrew_emel@hotmail.com>
2018-08-29 09:49:56 +0200

    fix documentation files

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'rinoh.frontend.sphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
