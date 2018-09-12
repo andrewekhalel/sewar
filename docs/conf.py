46deb403c71c5d3682ac7b793d55e27a03d07090
andrewekhalel <andrew_emel@hotmail.com>
2018-09-12 12:07:22 +0200

    v0.3

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, Andrew Khalel'
 author = 'Andrew Khalel'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
